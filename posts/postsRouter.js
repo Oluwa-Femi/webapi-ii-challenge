968799077bdc7214a2224ba84344944d4a85686f
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-10-30 08:17:38 +0000

    get and post request done

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -12,11 +12,46 @@ router.get('/', (req, res) => {
      .catch(error => {
           console.log(error)
           res.status(500).json({
-               message: "Error retrieving items"
+               message: "Error retrieving post"
+            })
+        })
+   })
+   
+   router.get('/:id', (req, res) => {
+        db.findById(req.params.id)
+        .then(post => {
+             res.status(200).json(post)
+        })
+        .catch(error => {
+             console.log(error)
+             res.status(500).json({
+                  message: "Error retrieving post"
           })
      })
 })
 
+router.get('/:id/comments', (req, res) => {
+    db.findPostComments(req.params.id)
+    .then(post => {
+         res.status(200).json(post)
+    })
+})
 
+router.post('/', (req, res)=> {
+    db.insert(req.body)
+    .then(post => {
+         res.status(201).json(post)
+    })
+    .catch(error => {
+         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    })
+})
+
+router.post('/:id/comments', (req, res)=> {
+    db.insertComment(req.body)
+    .then(comment => {
+         res.status(201).json(comment)
+    })
+})
 
 module.exports = router;
